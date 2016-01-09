6454327d300f2d5d53a9c6f10aeb737d1aabd05b
hito4t <thitoshi@cac.co.jp>
2016-01-09 09:45:28 +0900

    Support schema in SQLServer

--- embulk-input-sqlserver/src/main/java/org/embulk/input/sqlserver/SQLServerInputConnection.java
+++ embulk-input-sqlserver/src/main/java/org/embulk/input/sqlserver/SQLServerInputConnection.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 
 import org.embulk.input.jdbc.JdbcInputConnection;
 
+import com.google.common.base.Optional;
+
 public class SQLServerInputConnection extends JdbcInputConnection {
 
     public SQLServerInputConnection(Connection connection, String schemaName) throws SQLException
@@ -18,4 +20,27 @@ public class SQLServerInputConnection extends JdbcInputConnection {
         // NOP
     }
 
+    @Override
+    public String buildSelectQuery(String tableName,
+            Optional<String> selectColumnList, Optional<String> whereCondition,
+            Optional<String> orderByColumn)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("SELECT ");
+        sb.append(selectColumnList.or("*"));
+        sb.append(" FROM ");
+        if (schemaName != null) {
+            sb.append(quoteIdentifierString(schemaName)).append(".");
+        }
+        sb.append(quoteIdentifierString(tableName));
+        if (whereCondition.isPresent()) {
+            sb.append(" WHERE ").append(whereCondition.get());
+        }
+        if (orderByColumn.isPresent()) {
+            sb.append("ORDER BY ").append(quoteIdentifierString(orderByColumn.get())).append(" ASC");
+        }
+
+        return sb.toString();
+    }
 }
