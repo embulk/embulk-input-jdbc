02ba88f8e3e7138583e4670bc4c9e38198df3dad
joker1007 <kakyoin.hierophant@gmail.com>
2016-02-02 00:08:55 +0900

    Implement json type support

--- embulk-input-jdbc/src/main/java/org/embulk/input/jdbc/getter/StringColumnGetter.java
+++ embulk-input-jdbc/src/main/java/org/embulk/input/jdbc/getter/StringColumnGetter.java
@@ -4,12 +4,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.embulk.spi.Column;
 import org.embulk.spi.PageBuilder;
+import org.embulk.spi.json.JsonParseException;
+import org.embulk.spi.json.JsonParser;
 import org.embulk.spi.type.Type;
 import org.embulk.spi.type.Types;
+import org.msgpack.value.Value;
 
 public class StringColumnGetter
         extends AbstractColumnGetter
 {
+    private static final JsonParser jsonParser = new JsonParser();
+
     private String value;
 
     public StringColumnGetter(PageBuilder to, Type toType)
@@ -53,6 +58,19 @@ public class StringColumnGetter
             return;
         }
         to.setDouble(column, d);
+    }
+
+    @Override
+    public void jsonColumn(Column column)
+    {
+        Value v;
+        try {
+            v = jsonParser.parse(value);
+        } catch (JsonParseException e) {
+            super.jsonColumn(column);
+            return;
+        }
+        to.setJson(column, v);
     }
 
     @Override
