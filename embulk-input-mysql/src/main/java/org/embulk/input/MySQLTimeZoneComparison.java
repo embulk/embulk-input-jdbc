eb2ae73e5ee2bd4248fa5318d9c433e91dfc7f35
Hiroyuki Sato <hiroysato@gmail.com>
2017-05-17 15:15:51 +0900

    Change error message. add period

--- embulk-input-mysql/src/main/java/org/embulk/input/MySQLTimeZoneComparison.java
+++ embulk-input-mysql/src/main/java/org/embulk/input/MySQLTimeZoneComparison.java
@@ -35,7 +35,7 @@ public class MySQLTimeZoneComparison
         }
 
         if( serverTimeZone == null ){
-            logger.warn("Can't get server TimeZone");
+            logger.warn("Can't get server TimeZone.");
             return;
         }
 
