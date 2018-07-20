3b4fbcc0688d40dafc18d1433c50fcd4de80c6c8
sakama <satoshiakama@gmail.com>
2018-07-20 14:21:46 +0900

    Add test cases for use_raw_query_with_incremental:true with multi columns

--- embulk-input-postgresql/src/test/resources/org/embulk/input/postgresql/test/expect/incremental/query/setup.sql
+++ embulk-input-postgresql/src/test/resources/org/embulk/input/postgresql/test/expect/incremental/query/setup.sql
@@ -2,12 +2,12 @@ drop table if exists query_load;
 
 create table query_load (
     num int not null,
+    num2 int not null,
     note text
 );
 
-insert into query_load (num, note) values
-(3, 'first'),
-(4, 'first'),
-(2, 'first'),
-(1, 'first');
-
+insert into query_load (num, num2, note) values
+(3, 103, 'first'),
+(4, 104, 'first'),
+(2, 102, 'first'),
+(1, 101, 'first');
--- embulk-input-redshift/src/test/resources/org/embulk/input/redshift/test/expect/incremental/query/insert_more.sql
+++ embulk-input-redshift/src/test/resources/org/embulk/input/redshift/test/expect/incremental/query/insert_more.sql
@@ -1,6 +1,6 @@
 
-insert into query_load (num, note) values
-(0, 'more_skip'),
-(4, 'more_skip'),
-(9, 'more_load'),
-(5, 'more_load');
+insert into query_load (num, num2, note) values
+(0, 100, 'more_skip'),
+(4, 104, 'more_skip'),
+(9, 109, 'more_load'),
+(5, 105, 'more_load');
