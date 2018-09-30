23465d2e73a6ff485d6f75a94cd478b76d9bce49
TAC <tac@tac42.net>
2018-09-30 11:40:13 +0900

    Implement newConnection for only just working

--- embulk-input-clickhouse/src/main/java/org/embulk/input/ClickHouseInputPlugin.java
+++ embulk-input-clickhouse/src/main/java/org/embulk/input/ClickHouseInputPlugin.java
@@ -1,13 +1,16 @@
 package org.embulk.input;
 
-
 import com.google.common.base.Optional;
-import org.embulk.input.jdbc.JdbcInputConnection;
+import com.google.common.base.Throwables;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
 import org.embulk.input.jdbc.AbstractJdbcInputPlugin;
+import org.embulk.input.jdbc.JdbcInputConnection;
 
+import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ClickHouseInputPlugin
         extends AbstractJdbcInputPlugin
@@ -23,35 +26,19 @@ public class ClickHouseInputPlugin
         public String getHost();
 
         @Config("port")
-        @ConfigDefault("5432")
+        @ConfigDefault("8123")
         public int getPort();
 
         @Config("user")
-        public String getUser();
+        @ConfigDefault("null")
+        public Optional<String> getUser();
 
         @Config("password")
-        @ConfigDefault("\"\"")
-        public String getPassword();
+        @ConfigDefault("null")
+        public Optional<String> getPassword();
 
         @Config("database")
         public String getDatabase();
-
-        @Config("schema")
-        @ConfigDefault("\"public\"")
-        public String getSchema();
-
-        @Config("ssl")
-        @ConfigDefault("false")
-        public boolean getSsl();
-
-        @Config("application_name")
-        @ConfigDefault("\"embulk-input-clickhouse\"")
-        public String getApplicationName();
-    }
-
-    @Override
-    protected JdbcInputConnection newConnection(PluginTask task) throws SQLException {
-        return null;
     }
 
     @Override
@@ -60,4 +47,45 @@ public class ClickHouseInputPlugin
         return ClickHousePluginTask.class;
     }
 
+
+    @Override
+    protected JdbcInputConnection newConnection(PluginTask task) throws SQLException {
+        final String DriverClass = "ru.yandex.clickhouse.ClickHouseDriver";
+
+        ClickHousePluginTask t = (ClickHousePluginTask) task;
+
+        loadDriver(DriverClass, t.getDriverPath());
+
+        Properties props = new Properties();
+        if (t.getUser().isPresent()) {
+            props.setProperty("user", t.getUser().get());
+        }
+        if (t.getPassword().isPresent()) {
+            props.setProperty("password", t.getPassword().get());
+        }
+
+        props.putAll(t.getOptions());
+
+        Driver driver;
+        try {
+            // TODO check Class.forName(driverClass) is a Driver before newInstance
+            //      for security
+            driver = (Driver) Class.forName(DriverClass).newInstance();
+        } catch (Exception ex) {
+            throw Throwables.propagate(ex);
+        }
+
+        final String url = String.format("jdbc:clickhouse://%s:%d/%s", t.getHost(), t.getPort(), t.getDatabase());
+
+        Connection con = driver.connect(url, props);
+        try {
+            JdbcInputConnection c = new JdbcInputConnection(con, null);
+            con = null;
+            return c;
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
 }
