8b40911c31375327df9e7afb1d3c85d4218a6174
ommadawn46 <ommadawn46@gmail.com>
2019-05-15 20:20:21 +0900

    Fix missing left brace when getting empty array column

--- embulk-input-postgresql/src/main/java/org/embulk/input/postgresql/getter/ArrayColumnGetter.java
+++ embulk-input-postgresql/src/main/java/org/embulk/input/postgresql/getter/ArrayColumnGetter.java
@@ -155,7 +155,9 @@ public class ArrayColumnGetter
             }
             builder.append(",");
         }
-        builder.deleteCharAt(builder.length() - 1);
+        if (values.length > 0) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
         builder.append("}");
     }
 
--- embulk-input-postgresql/src/test/resources/org/embulk/input/postgresql/test/expect/array/setup.sql
+++ embulk-input-postgresql/src/test/resources/org/embulk/input/postgresql/test/expect/array/setup.sql
@@ -12,3 +12,5 @@ insert into input_array (c1, c2, c3, c4) values ('{1000, 2000, 3000, 4000}', '{{
 insert into input_array (c1, c2, c3, c4) values ('{5000, 6000, 7000, 8000}', '{{"yellow", "magenta"}, {"purple", "light,dark"}}', '{{{t,t},{f,f}},{{t,f},{f,t}}}', '{12345678901234567890}');
 
 insert into input_array (c1, c2, c3, c4) values ('{1000}', '{{"\"", "{\\}", "{a,b}"}}', '{true}', '{12345678901234567890.1234567890}');
+
+insert into input_array (c1, c2, c3, c4) values ('{}', '{}', '{}', '{}');
