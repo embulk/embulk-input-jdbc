f5ab0a3658a4e2eae210299cd8ca29fac8bc6ff6
Joe Vu <joe.vu@treasure-data.com>
2023-06-21 17:56:29 +0700

    refactor build SQLServer limit query

--- embulk-input-sqlserver/src/main/java/org/embulk/input/sqlserver/SQLServerPreviewQueryBuilder.java
+++ embulk-input-sqlserver/src/main/java/org/embulk/input/sqlserver/SQLServerPreviewQueryBuilder.java
@@ -1,11 +1,12 @@
 package org.embulk.input.sqlserver;
 
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class SQLServerPreviewQueryBuilder
 {
     private static final String TOP_STATEMENT_TEMPLATE = "%s TOP 100 %s";
-    private static final Pattern SELECT_PATTERN = Pattern.compile("select", Pattern.CASE_INSENSITIVE);
 
     private String query;
 
@@ -16,7 +17,11 @@ public class SQLServerPreviewQueryBuilder
 
     public String build()
     {
-        final String[] statements = SELECT_PATTERN.split(query);
-        return String.format(TOP_STATEMENT_TEMPLATE, "SELECT", statements[1]);
+        final List<String> queryTokens = Pattern.compile("\\s+")
+            .splitAsStream(query)
+            .collect(Collectors.toList());
+        queryTokens.remove(0);
+
+        return String.format(TOP_STATEMENT_TEMPLATE, "SELECT", queryTokens.stream().collect(Collectors.joining(" ")));
     }
 }
